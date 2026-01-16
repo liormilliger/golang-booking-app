7c9d2d2476a0f477750c1c9c72eee702b10fd06c
Lior Milliger <liormilliger@gmail.com>
2026-01-16 15:51:47 +0200

    Validating user input

--- main.go
+++ main.go
@@ -34,7 +34,11 @@ func main() {
 		fmt.Println("How many tickets do you want? ")
 		fmt.Scan(&userTickets)
 
-		if userTickets <= remainingTickets {
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, firstName+" "+lastName)
 
@@ -61,7 +65,7 @@ func main() {
 			}
 
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			fmt.Println("Your input data is invalid. Try again.")
 		}
 
 	}
