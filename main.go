f46ecb8c1f50cecf739f9ced7cd582a3662cfab3
Lior Milliger <liormilliger@gmail.com>
2026-01-16 09:03:26 +0200

    for each

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -43,7 +46,11 @@ func main() {
 		fmt.Printf("You will receive a confirmation email at %v\n", email)
 
 		fmt.Printf("%v remaining tickets for the %v\n", remainingTickets, conferenceName)
-
-		fmt.Printf("These are all our bookings %v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 	}
 }
